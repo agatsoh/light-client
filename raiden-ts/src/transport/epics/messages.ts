7dd2511ca3152147a75f21cfd3c24b56e9da1179
André Vitor de Lima Matos <andre.vmatos@gmail.com>
2021-03-15 23:57:41 -0300

    sdk: also send global messages to config.additionalServices

--- raiden-ts/src/transport/epics/messages.ts
+++ raiden-ts/src/transport/epics/messages.ts
@@ -1,4 +1,5 @@
-import isEmpty from 'lodash/isEmpty';
+import constant from 'lodash/constant';
+import uniq from 'lodash/uniq';
 import type { MatrixEvent } from 'matrix-js-sdk';
 import type { Observable } from 'rxjs';
 import {
@@ -29,6 +30,7 @@ import {
   take,
   takeUntil,
   tap,
+  toArray,
   withLatestFrom,
 } from 'rxjs/operators';
 
@@ -40,6 +42,7 @@ import type { Delivered, Message } from '../../messages/types';
 import { MessageType, Processed, SecretRequest, SecretReveal } from '../../messages/types';
 import { encodeJsonMessage, isMessageReceivedOfType, signMessage } from '../../messages/utils';
 import { ServiceDeviceId } from '../../services/types';
+import { pfsInfoAddress } from '../../services/utils';
 import type { RaidenState } from '../../state';
 import type { Latest, RaidenEpicDeps } from '../../types';
 import { isActionOf } from '../../utils/actions';
@@ -47,7 +50,7 @@ import { assert, networkErrors } from '../../utils/error';
 import { LruCache } from '../../utils/lru';
 import { getServerName } from '../../utils/matrix';
 import { completeWith, mergeWith, retryWhile } from '../../utils/rx';
-import { Signed } from '../../utils/types';
+import { Address, Signed } from '../../utils/types';
 import type { Presences } from '../types';
 import { getCap, getPresenceByUserId } from '../utils';
 import { parseMessage } from './helpers';
@@ -233,18 +236,31 @@ export function matrixMessageSendEpic(
   return merge(sendToRtc$, sendToDevice$);
 }
 
-function sendServiceMessage(
-  request: messageServiceSend.request,
-  { matrix$, config$, latest$ }: Pick<RaidenEpicDeps, 'matrix$' | 'config$' | 'latest$'>,
-) {
+function sendServiceMessage(request: messageServiceSend.request, deps: RaidenEpicDeps) {
+  const { matrix$, config$, latest$ } = deps;
   return matrix$.pipe(
-    withLatestFrom(latest$),
-    mergeMap(([matrix, { state }]) => {
-      assert(!isEmpty(state.services), 'no services to messageServiceSend to');
-      const serverName = getServerName(matrix.getHomeserverUrl());
-      const userIds = Object.keys(state.services).map(
-        (service) => `@${service.toLowerCase()}:${serverName}`,
+    withLatestFrom(latest$, config$),
+    mergeWith(([, , { additionalServices }]) =>
+      from(additionalServices).pipe(
+        mergeMap(
+          (serviceAddrOrUrl) =>
+            Address.is(serviceAddrOrUrl)
+              ? of(serviceAddrOrUrl)
+              : defer(async () => pfsInfoAddress(serviceAddrOrUrl, deps)).pipe(
+                  catchError(constant(EMPTY)),
+                ),
+          5,
+        ),
+        toArray(),
+      ),
+    ),
+    mergeMap(([[matrix, { state }], additionalServicesAddrs]) => {
+      const servicesAddrs = uniq(
+        additionalServicesAddrs.concat(Object.keys(state.services) as Address[]),
       );
+      assert(servicesAddrs.length, 'no services to messageServiceSend to');
+      const serverName = getServerName(matrix.getHomeserverUrl());
+      const userIds = servicesAddrs.map((service) => `@${service.toLowerCase()}:${serverName}`);
       // batch action messages in a single text body
       const content = {
         msgtype: 'm.text',
